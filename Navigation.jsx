8c2d8294e35662df560bb0685f844a48e26ba188
vyshu2714-commits <vyshu2714@gmail.com>
2026-05-02 11:55:14 +0530

    Add files via upload

---
+++ Navigation.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Navigation() {
+  return (
+    <nav>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
